7e18b35ee1de05bcb22bd4c917985fe6947bf50d
Mercy Oyelude <mercyoyelude0@gmail.com>
2023-11-27 23:09:12 +0100

    Updated the code.

--- app.js
+++ app.js
@@ -38,3 +38,54 @@ function toggleOptionCards() {
         arrowUp.style.display = 'block';
     }
 }
+
+function toggleOptionOne() {
+    let optionOneText = document.getElementById('optionOneText');
+    let optionOneButton = document.getElementById('customize-theme');
+    let optionOneRight = document.getElementById('optionOneRight');
+
+    optionOneText.classList.toggle("hide");
+    optionOneButton.classList.toggle("hide");
+    optionOneRight.classList.toggle("hide");
+}
+
+function toggleOptionTwo() {
+    let optionTwoText = document.getElementById('optionTwoText');
+    let optionTwoButton = document.getElementById('add-product');
+    let importProductButton = document.getElementById('import-product');
+    let optionTwoRight = document.getElementById('optionTwoRight');
+
+    optionTwoText.classList.toggle("hide");
+    optionTwoButton.classList.toggle("hide");
+    importProductButton.classList.toggle("hide");
+    optionTwoRight.classList.toggle("hide");
+}
+
+function toggleOptionThree() {
+    let optionThreeText = document.getElementById('optionThreeText');
+    let optionThreeButton = document.getElementById('add-domain');
+    let optionThreeRight = document.getElementById('optionThreeRight');
+
+    optionThreeText.classList.toggle("hide");
+    optionThreeButton.classList.toggle("hide");
+    optionThreeRight.classList.toggle("hide");
+}
+
+function toggleOptionFour() {
+    let optionFourText = document.getElementById('optionFourText');
+    let optionFourButton = document.getElementById('name-store');
+    let optionFourRight = document.getElementById('optionFourRight');
+
+    optionFourText.classList.toggle("hide");
+    optionFourButton.classList.toggle("hide");
+    optionFourRight.classList.toggle("hide");
+}
+function toggleOptionFive() {
+    let optionFiveText = document.getElementById('optionFiveText');
+    let optionFiveButton = document.getElementById('setup-payment');
+    let optionFiveRight = document.getElementById('optionFiveRight');
+
+    optionFiveText.classList.toggle("hide");
+    optionFiveButton.classList.toggle("hide");
+    optionFiveRight.classList.toggle("hide");
+}
